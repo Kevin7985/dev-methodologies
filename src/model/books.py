5a0aaa4727bc84d73f71a33ede813ce1d1eb3dd0
Kevin7985 <1298282@mail.ru>
2023-10-08 16:17:55 +0300

    add genres, authors, book_genre and book_author

--- src/model/books.py
+++ src/model/books.py
@@ -9,20 +9,35 @@ from src.database import Base
 
 class Book(Base):
     __tablename__ = "books"
-    guid = Column(UUIDType(binary=False), nullable=False, unique=True, primary_key=True, index=True, default=uuid.uuid4)
-    name = Column(String, comment="Название книги")
-    authors = Column(JSONB, comment="Список авторов")
-    publication_date = Column(DateTime, comment="Дата публикации")
+    book_id = Column(UUIDType(binary=False), nullable=False, unique=True, primary_key=True, index=True, default=uuid.uuid4)
+    title = Column(String, comment="Название книги")
+    description = Column(String, comment="Описание книги")
     rating = Column(Float, comment="Оценка пользователей")
-    quantity = Column(Integer, default=0, comment="Количество экземпляров")
-    cover = Column(String, comment="Обложка книги")
-    isbn = Column(String, comment="ISBN - книги")
-    genre = Column(JSONB, comment="Жанры")
+    isbn = Column(String, comment="IBSN книги")
+    pic_file_name = Column(String, comment="Обложка книги")
 
 
 class Author(Base):
     __tablename__ = "authors"
-    guid = Column(UUIDType(binary=False), nullable=False, unique=True, primary_key=True, index=True, default=uuid.uuid4)
-    name = Column(String, comment="Имя")
-    last_name = Column(String, comment="Фамилия")
-    patronymic = Column(String, comment="Отчество")
+    author_id = Column(UUIDType(binary=False), nullable=False, unique=True, primary_key=True, index=True, default=uuid.uuid4)
+    name = Column(String, comment="Имя автора")
+    surname = Column(String, comment="Фамилия автора")
+    patronymic = Column(String, comment="Отчество автора")
+
+
+class Genre(Base):
+    __tablename__ = "genres"
+    genre_id = Column(UUIDType(binary=False), nullable=False, unique=True, primary_key=True, index=True, default=uuid.uuid4)
+    title = Column(String, comment="Название жанра")
+
+
+class Book_Author(Base):
+    __tablename__ = "book_author"
+    book_id = Column(UUIDType(binary=False), nullable=False, default=uuid.uuid4)
+    author_id = Column(UUIDType(binary=False), nullable=False, default=uuid.uuid4)
+
+
+class Book_Genre(Base):
+    __tablename__ = "book_genre"
+    book_id = Column(UUIDType(binary=False), nullable=False, default=uuid.uuid4)
+    genre_id = Column(UUIDType(binary=False), nullable=False, default=uuid.uuid4)
