34a973d5fd60180ca2989e0258560703c287593b
Kevin7985 <1298282@mail.ru>
2023-10-11 22:54:19 +0300

    get book by id

--- src/api/v1/books.py
+++ src/api/v1/books.py
@@ -1,6 +1,7 @@
+import uuid
 from typing import Annotated
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, HTTPException
 from fastapi_filter import FilterDepends
 from fastapi_pagination import Page
 from fastapi_pagination.ext.sqlalchemy import paginate
@@ -22,6 +23,21 @@ _CollectionOfOfferFilter = Annotated[BookListFilter, FilterDepends(BookListFilte
 @router.get("/all", summary="Список всех книг", response_model=Page[Book])
 async def get_all(db: DB, book_filter: _CollectionOfOfferFilter, author_name: str | None = DEFAULT_AUTHOR_STRING):
     return await paginate(db, crud_book.get_filtered(book_filter=book_filter, author_name=author_name), unique=False)
+
+
+@router.get("/{book_id}", summary="Получение книги по guid", response_model=Book)
+async def get_book_by_id(db: DB, book_id):
+    book_uuid = None
+    try:
+        book_uuid = uuid.UUID(book_id)
+    except:
+        raise HTTPException(status_code=400, detail="Invalid book_id")
+    
+    result = await crud_book.get(db, book_uuid)
+    if not result:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return result
 
 
 @router.get("/authors", summary="Список всех авторов", response_model=list[Author])
