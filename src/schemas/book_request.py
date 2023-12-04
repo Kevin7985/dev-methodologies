05abfefd451630a694c7d2e2fa1871c89e210159
Kevin7985 <1298282@mail.ru>
2023-12-04 20:04:49 +0300

    book requests initial commit

---
+++ src/schemas/book_request.py
@@ -0,0 +1,36 @@
+from uuid import UUID
+from datetime import datetime
+
+from pydantic import BaseModel
+from fastapi_filter.contrib.sqlalchemy import Filter
+
+from src.model.book_requests import BookRequest as m_BookRequest
+from src.utils.const import BookRequestStatusEnum
+
+
+class BookRequestBase(BaseModel):
+    user_id: UUID
+    book_id: UUID
+    point_id: UUID
+    status: BookRequestStatusEnum
+
+
+class BookRequest(BookRequestBase):
+    guid: UUID
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class BookRequestListFilter(Filter):
+    user_id: UUID | None
+    book_id: UUID | None
+    point_id: UUID | None
+    status: BookRequestStatusEnum | None
+
+    class Constants(Filter.Constants):
+        model = m_BookRequest
+
+    class Config:
+        allow_population_by_field_name=True
